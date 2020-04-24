aed99fc8a056e892288a24794c60cf8ff435d801
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-04-24 23:24:33 +0530

    Updates

---
+++ precache-manifest.b4b131577d8873004fde8b1f7feab381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a25625e13d1a0a8f645425a0d89a8fd",
+    "url": "/Nike/index.html"
+  },
+  {
+    "revision": "363fc730f843fa970859",
+    "url": "/Nike/static/css/main.fb0316b9.chunk.css"
+  },
+  {
+    "revision": "7f22acf8ea8a1a37b289",
+    "url": "/Nike/static/js/2.6f41c62b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Nike/static/js/2.6f41c62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363fc730f843fa970859",
+    "url": "/Nike/static/js/main.52317dc6.chunk.js"
+  },
+  {
+    "revision": "90eaca1ef7b7708bbb47",
+    "url": "/Nike/static/js/runtime-main.deb4156b.js"
+  }
+]);
